6f90a30a04e892a76fe3362044e4cc4a9894dc53
Terribly-Lost-Cause <limcavell9@gmail.com>
2022-01-31 14:34:54 +0800

    dashboard

--- Recyclables/routes/dashboard.js
+++ Recyclables/routes/dashboard.js
@@ -13,32 +13,42 @@ router.get('/main', async function(req, res) {
 
     var checkValidatorSession = await require("../utils/validation_session")(req.session.userId, req.cookies.new_cookie)
 
-    if (checkValidatorSession == "false"){
+    if (checkValidatorSession == "false") {
         res.redirect('/user/login')
-    }
-    else if (checkValidatorSession == "true"){
+    } else if (checkValidatorSession == "true") {
 
         var checkValidatorUser = await require("../utils/validation_user")(req.session.userId)
-        Bin.findAll({}).then(bin => { //find all recyclables bins available
+        Bin.findAll().then(bin => { //find all recyclables bins available
             const oldbinlist = bin;
-            
+            let updatedstatus = null
+
             for (var i = 0; i < oldbinlist.length; i++) {
+                let binid = oldbinlist[i].bin_id
                 let status = oldbinlist[i].status
                 let curPlastic = oldbinlist[i].current_plastic
                 let curMetal = oldbinlist[i].current_metal
                 let threshold = oldbinlist[i].threshold
-                let updatedstatus = null
 
                 let plastic_level = curPlastic / threshold * 100
                 let metal_level = curMetal / threshold * 100
-                if(status != 0){
-                    if(plastic_level < 50 && metal_level<50){
-                    updatedstatus = 1
+                if (status != 0) {
+                    if (plastic_level < 50 && metal_level < 50) {
+                        updatedstatus = 1
+                    } else if ((plastic_level >= 50 && plastic_level < 75) || (metal_level >= 50 && metal_level < 75)) {
+                        updatedstatus = 2
+                    } else if ((plastic_level >= 75) || (metal_level >= 75)) {
+                        updatedstatus = 3
+                    }
+                    Bin.update({
+                        status: updatedstatus
+                    }, {
+                        where: {
+                            bin_id: binid
+                        }
+                    })
                 }
-                }
-                
-                console.log(">>>>>>>>>>>>>>>>>>>>",oldbinlist[i].current_plastic)
-                console.log(">>>>>>>>>>>>>>>>>>>>",oldbinlist[i].current_metal)
+
+
             }
         })
         Bin.findAll({}).then(bin => { //find all recyclables bins available
@@ -57,20 +67,19 @@ router.get('/main', async function(req, res) {
                 }
             }
 
-            
 
-            if (checkValidatorUser == "cleaner"){
+
+            if (checkValidatorUser == "cleaner") {
                 res.render('user/dashboard', { //render page
                     binlist: binlist
                 })
-            }
-            else if (checkValidatorUser == "supervisor"){
+            } else if (checkValidatorUser == "supervisor") {
                 res.render('user/dashboard', { //render page
                     binlist: binlist,
                     type: "supervisor"
                 })
             }
-            
+
         })
     }
 });
